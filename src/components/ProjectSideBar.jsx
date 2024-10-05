8edf7ebfcd6f2cf25e5e7b97c66e1573390b5ed9
Your Name <you@example.com>
2024-10-05 16:18:38 +0200

    add pe

--- src/components/ProjectSideBar.jsx
+++ src/components/ProjectSideBar.jsx
@@ -2,7 +2,7 @@ import LogOutMenu from "./LogOutMenu";
 
 export default function ProjectSideBar({ children, users }) {
 	return (
-		<section className="h-content flex flex-col md:flex-row w-4/12 pb-2 bg-stone-700 text-stone-100 relative">
+		<section className="h-content flex flex-col md:flex-row w-4/12 pe-0.5 pb-2 bg-stone-700 text-stone-100 relative">
 			<div className="flex flex-col px-4 justify-space mt-6 h-screen">
 				<h2 className="uppercase font-bold mt-6 mb-1 text-xl">Lista działów</h2>
 				<ul className="list-disc px-2">{children}</ul>
