983cb81d1a448a651d19138cd2b7ffce13474cd9
Your Name <you@example.com>
2024-10-05 15:53:58 +0200

    change width of ProjectSideBar

--- src/components/ProjectSideBar.jsx
+++ src/components/ProjectSideBar.jsx
@@ -2,8 +2,8 @@ import LogOutMenu from "./LogOutMenu";
 
 export default function ProjectSideBar({ children, users }) {
 	return (
-		<section className="flex  w-6/12 md:w-4/12 bg-stone-700 text-stone-100 relative">
-			<div className="flex flex-col px-4 justify-space mt-4 h-screen">
+		<section className="flex flex-col md:flex-row w-6/12 md:w-4/12 bg-stone-700 text-stone-100 relative">
+			<div className="flex flex-col px-4 justify-space mt-6 h-screen">
 				<h2 className="uppercase font-bold mt-6 mb-1 text-xl">Lista działów</h2>
 				<ul className="list-disc px-2">{children}</ul>
 			</div>
